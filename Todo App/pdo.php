283498df68ffd6e9daf5dc232e4ed0dbc3848e90
Santhosh Kumar <santelee99@gmail.com>
2020-11-27 18:30:38 +0530

    Delete pdo.php

--- Todo App/pdo.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=proj', 
-   'sandy', '123');
-// See the "errors" folder for details...
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
